be705e0e366896e063d6cec7010eac76926c7fef
DHANUwa <pdhanushka856@gmail.com>
2026-02-14 10:42:15 +0530

    update my cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import { ArrowRight, Download } from 'lucide-react';
+import myCv from '/my_professional_cv.pdf';
 
 const Hero = () => {
   // Animation Variants (කෝඩ් එක පිරිසිදුව තබා ගැනීමට)
@@ -93,9 +94,11 @@ const Hero = () => {
           </a>
 
           {/* Secondary Button */}
-          <a 
-  href="/my_professional_cv.pdf" // public folder එකේ තියෙන file එකේ නම
-  download="Dhanushka_CV.pdf" // download වෙද්දී වැටිය යුතු නම
+       <a 
+  href={myCv} 
+  download="Dhanushka_CV.pdf" 
+  target="_blank"
+  rel="noopener noreferrer"
   className="group px-8 py-3 w-full sm:w-auto rounded-full border border-white/10 bg-white/5 backdrop-blur-lg font-bold text-slate-300 flex items-center justify-center gap-2 hover:bg-white/10 hover:text-white transition-all hover:scale-105 active:scale-95 cursor-pointer text-center"
 >
   Download CV 
